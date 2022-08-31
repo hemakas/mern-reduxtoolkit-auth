cefe5961d4a58dfc27f06f38d4f1e5d10851844c
hemakas <hemaka.tropusi@gmail.com>
2022-09-01 00:11:30 +0530

    backend sign in feature added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,7 @@ import User from '../models/User'
 
 const secret = 'test'
 
-// sign in
+// sign up function
 export const SignUp = async (req, res) => {
     const { name, email, password } = req.body
 
@@ -27,7 +27,7 @@ export const SignUp = async (req, res) => {
 
         })
 
-        // sign in token
+        // sign up token
         const token = jwt.sign({
             email: result.email,
             id: result._id
@@ -41,3 +41,38 @@ export const SignUp = async (req, res) => {
         console.log(error)
     }
 }
+
+// sign in function
+export const SignIn = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const registeredUser = await User.findOne({ email })
+
+        if (!registeredUser) {
+            return res.status(404).json({ message: 'User doesnt exist' })
+        }
+
+        const correctPassword = await bcrypt.compare(password, registeredUser.password)
+
+        // check if the password enters matches with the saved password
+        if (!correctPassword) {
+            return res.status(400).json({ message: 'Invalid credentials' })
+        }
+
+        // sign in token
+        const token = jwt.sign({ 
+            email: registeredUser.email, 
+            id: registeredUser._id
+        }, secret, {
+            expiresIn: '1h'
+        })
+
+        res.status(200).json({ result: registeredUser, token })
+
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong'})
+        console.log(error)
+    }
+}
+
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import { SignUp } from '../controllers/userController'
+import { SignUp, SignIn } from '../controllers/userController'
 
 const router = express.Router()
 
+// sign up
 router.post('/signup', SignUp)
+
+// sign in
+router.post('/signin', SignIn)
 
 export default router
