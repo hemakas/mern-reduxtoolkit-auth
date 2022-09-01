d381e70b56d2198e0b7d77eeebc1f50d8b444724
hemakas <hemaka.tropusi@gmail.com>
2022-09-01 10:35:40 +0530

    taostify configuration added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import './App.css'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer />
       <h2>Hello react</h2>
     </div>
   );
